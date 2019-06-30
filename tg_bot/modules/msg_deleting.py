5a71c5e7ae129c2e183232a544e5c9b3665fd9a2
Stark <49307071+bagus02@users.noreply.github.com>
2019-06-30 09:34:02 +0700

    add purge check

--- tg_bot/modules/msg_deleting.py
+++ tg_bot/modules/msg_deleting.py
@@ -1,4 +1,4 @@
-import html
+import html, time
 from typing import Optional, List
 
 from telegram import Message, Chat, Update, Bot, User
@@ -7,18 +7,15 @@ from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import mention_html
 
-from emilia import dispatcher, LOGGER, spamfilters
-from emilia.modules.helper_funcs.chat_status import user_admin, can_delete
-from emilia.modules.log_channel import loggable
+from tg_bot import dispatcher, LOGGER
+from tg_bot.modules.helper_funcs.chat_status import user_admin, can_delete
+from tg_bot.modules.log_channel import loggable
 
 
 @run_async
 @user_admin
 @loggable
 def purge(bot: Bot, update: Update, args: List[str]) -> str:
-    spam = spamfilters(update.effective_message.text, update.effective_message.from_user.id, update.effective_chat.id, update.effective_message)
-    if spam == True:
-        return
     msg = update.effective_message  # type: Optional[Message]
     if msg.reply_to_message:
         user = update.effective_user  # type: Optional[User]
@@ -34,8 +31,8 @@ def purge(bot: Bot, update: Update, args: List[str]) -> str:
                     bot.deleteMessage(chat.id, m_id)
                 except BadRequest as err:
                     if err.message == "Message can't be deleted":
-                        bot.send_message(chat.id, "Tidak dapat menghapus semua pesan. Pesannya mungkin terlalu lama, saya mungkin "
-                                                  "tidak memiliki hak menghapus, atau ini mungkin bukan supergrup.")
+                        bot.send_message(chat.id, "Cannot delete all messages. The messages may be too old, I might "
+                                                  "not have delete rights, or this might not be a supergroup.")
 
                     elif err.message != "Message to delete not found":
                         LOGGER.exception("Error while purging chat messages.")
@@ -44,22 +41,24 @@ def purge(bot: Bot, update: Update, args: List[str]) -> str:
                 msg.delete()
             except BadRequest as err:
                 if err.message == "Message can't be deleted":
-                    bot.send_message(chat.id, "Tidak dapat menghapus semua pesan. Pesannya mungkin terlalu lama, saya mungkin "
-                                              "tidak memiliki hak menghapus, atau ini mungkin bukan supergrup.")
+                    bot.send_message(chat.id, "Cannot delete all messages. The messages may be too old, I might "
+                                              "not have delete rights, or this might not be a supergroup.")
 
                 elif err.message != "Message to delete not found":
                     LOGGER.exception("Error while purging chat messages.")
 
-            bot.send_message(chat.id, "Pembersihan selesai.")
+            del_msg = bot.send_message(chat.id, "Purge complete.")
             return "<b>{}:</b>" \
                    "\n#PURGE" \
                    "\n<b>Admin:</b> {}" \
-                   "\nDibersihkan <code>{}</code> pesan.".format(html.escape(chat.title),
+                   "\nPurged <code>{}</code> messages.".format(html.escape(chat.title),
                                                                mention_html(user.id, user.first_name),
                                                                delete_to - message_id)
+            time.sleep(2)
+            bot.delete_message(chat.id, del_msg.id)
 
     else:
-        msg.reply_text("Balas pesan untuk memilih tempat mulai membersihkan.")
+        msg.reply_text("Reply to a message to select where to start purging from.")
 
     return ""
 
@@ -68,9 +67,6 @@ def purge(bot: Bot, update: Update, args: List[str]) -> str:
 @user_admin
 @loggable
 def del_message(bot: Bot, update: Update) -> str:
-    spam = spamfilters(update.effective_message.text, update.effective_message.from_user.id, update.effective_chat.id, update.effective_message)
-    if spam == True:
-        return
     if update.effective_message.reply_to_message:
         user = update.effective_user  # type: Optional[User]
         chat = update.effective_chat  # type: Optional[Chat]
@@ -80,22 +76,22 @@ def del_message(bot: Bot, update: Update) -> str:
             return "<b>{}:</b>" \
                    "\n#DEL" \
                    "\n<b>Admin:</b> {}" \
-                   "\nPesan dihapus.".format(html.escape(chat.title),
+                   "\nMessage deleted.".format(html.escape(chat.title),
                                                mention_html(user.id, user.first_name))
     else:
-        update.effective_message.reply_text("Apa yang ingin di hapus?")
+        update.effective_message.reply_text("Whadya want to delete?")
 
     return ""
 
 
 __help__ = """
-*Hanya admin:*
- - /del: menghapus pesan yang Anda balas
- - /purge: menghapus semua pesan antara ini dan membalas pesan.
- - /purge <integer X>: menghapus pesan yang dijawab, dan pesan X yang mengikutinya.
+*Admin only:*
+ - /del: deletes the message you replied to
+ - /purge: deletes all messages between this and the replied to message.
+ - /purge <integer X>: deletes the replied message, and X messages following it.
 """
 
-__mod_name__ = "Membersihkan"
+__mod_name__ = "Purges"
 
 DELETE_HANDLER = CommandHandler("del", del_message, filters=Filters.group)
 PURGE_HANDLER = CommandHandler("purge", purge, filters=Filters.group, pass_args=True)
